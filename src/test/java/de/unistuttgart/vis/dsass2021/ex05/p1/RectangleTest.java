0ec65d6224f5629d44edb5bc1b4ca18da000cdba
Lukas Müller <st174282@stud.uni-stuttgart.de>
2021-06-03 12:31:53 +0200

    tests

--- src/test/java/de/unistuttgart/vis/dsass2021/ex05/p1/RectangleTest.java
+++ src/test/java/de/unistuttgart/vis/dsass2021/ex05/p1/RectangleTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,5 +14,20 @@ import de.unistuttgart.vis.dsass2021.ex05.p1.Point;
 import de.unistuttgart.vis.dsass2021.ex05.p1.Rectangle;
 
 public class RectangleTest {
+	@Test
+	public void testExcludeAllPoints() {
+		Rectangle r1 = new Rectangle(100, 100, 100, 100);
+		ArrayList<Point> pointlist = new ArrayList<>();
+		Point pointOne = new Point(180, 150);
+		Point pointTwo = new Point(150, 180);
+		Point pointThree = new Point(101, 150);
+		Point pointFour = new Point(101, 149);
 
+		pointlist.add(pointOne);
+		pointlist.add(pointTwo);
+		// pointlist.add(pointThree);
+		// pointlist.add(pointFour);
+		assertEquals(79, (int) r1.excludePoints(pointlist).getWidth());
+		assertEquals(79, (int) r1.excludePoints(pointlist).getHeight());
+	}
 }
--- src/test/java/de/unistuttgart/vis/dsass2021/ex05/p1/SimpleQuadTreeTest.java
+++ src/test/java/de/unistuttgart/vis/dsass2021/ex05/p1/SimpleQuadTreeTest.java
@@ -19,5 +19,33 @@ import de.unistuttgart.vis.dsass2021.ex05.p1.SimpleQuadTree;
 
 
 public class SimpleQuadTreeTest {
+	@Test
+    public void boundingboxTest(){
 
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(3,4);
+        Point p3 = new Point(4,3);
+        Point p4 = new Point(67,3);
+        Point p5 = new Point(2,4);
+        Point p6 = new Point(6,4);
+        Point p7 = new Point(3,2);
+        Point p8 = new Point(21,2);
+        Point p9 = new Point(23,45);
+        Point p10 = new Point(29,29);
+
+        List<Point> elementList = new ArrayList<>();
+
+        elementList.add(p1);
+        elementList.add(p2);
+        elementList.add(p3);
+        elementList.add(p4);
+        elementList.add(p5);
+        elementList.add(p6);
+        elementList.add(p7);
+        elementList.add(p8);
+        elementList.add(p9);
+        elementList.add(p10);
+
+        SimpleQuadTree quadTree = new SimpleQuadTree(elementList,10);
+    }
 }
